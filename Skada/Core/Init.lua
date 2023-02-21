3d3451ee5c0a2973a1268d9eed4847f00043805a
Kader Bouyakoub <bkader@mail.com>
2023-02-21 23:03:13 +0100

    fix tooltip title not resetting when it should

--- Skada/Core/Init.lua
+++ Skada/Core/Init.lua
@@ -1772,6 +1772,7 @@ do
 
 	-- wipes window's dataset table
 	function Window:reset()
+		self.title = nil -- reset title
 		if not self.dataset then return end
 		for i = #self.dataset, 0, -1 do
 			if self.dataset[i] then
